f4e6dc690e290cc3ce851926a79e531f2df14fac
Bhupendra Singh <b76109dna@gmail.com>
2023-12-18 20:51:04 +0530

    second commit

--- script.js
+++ script.js
@@ -1,2 +1,78 @@
-// hello world 
- console.log("hello world");
+/* 
+1) You don't have to get or download JavaScript. JavaScript is already running in your browser on your computer, on your tablet, and on your smart-phone. JavaScript is free to use for everyone.
+
+2) JavaScript accepts both double and single quotes.
+
+3) JavaScript and Java are completely different languages, both in concept and design. JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997. ECMA-262 is the official name of the standard. ECMAScript is the official name of the language. The latest version of JavaScript will be ECMaScript2022 (ES13) in 2022.
+
+4) ECMAScript is a standard on which JavaScript is based. It was created to ensure that different documents on JavaScript are talking about the same language.
+
+5) What is JavaScript?
+   a) JavaScript is a lightweight, OOP language.
+   b) It is a scripting language for web pages.
+   c) It is used to add interactivity and dynamic effects to web pages. “.js” is the extension of javascript
+   d) Nowadays JS used in server-side development. JS Frontend Frameworks: React, Angular, Vue.
+      JS Backend Frameworks: Express, Node.
+
+6) JavaScript is a client-side language, which means that it is executed by the user's web browser rather than the web server. This allows JavaScript to interact with the user and create dynamic, interactive web pages. JavaScript is often used in combination with HTML and CSS to create web pages that are more interactive and engaging. It can be used to create all sorts of effects, such as drop-down menus, image sliders, and pop-up windows.
+
+7) Placing scripts at the bottom of the <body> element improves the display speed, because script interpretation slows down the display. External scripts are practical when the same code is used in many different web pages. External scripts cannot contain <script> tags.
+
+8) To access an HTML element, JavaScript can use the document.getElementById(id) method. Changing the innerHTML property of an HTML element is a common way to display data in HTML. The document.write() method should only be used for testing. For debugging purposes, you can call the console.log() method in the browser to display data.
+
+9) JavaScript does not have any print object or print methods. You cannot access output devices from JavaScript. The only exception is that you can call the window.print() method in the browser to print the content of the current window.
+
+10) JavaScript Programs:  A computer program is a list of "instructions" to be "executed" by a computer. In a programming language, these programming instructions are called statements. A JavaScript program is a list of programming statements. In HTML, JavaScript programs are executed by the web browser.
+
+11) JavaScript Statements: JavaScript statements are composed of: Values, Operators, Expressions,Keywords, and Comments. This statement tells the browser to write "Hello Dolly." Most JavaScript programs contain many JavaScript statements. The statements are executed, one by one, in the same order as they are written. JavaScript programs (and JavaScript statements) are often called JavaScript code. Semicolons (;) separate JavaScript statements. When separated by semicolons, multiple statements on one line are allowed.
+
+12) JavaScript Line Length and Line Breaks: For best readability, programmers often like to avoid code lines longer than 80 characters. If a JavaScript statement does not fit on one line, the best place to break it is after an operator like " = ":
+
+13) JavaScript Code Blocks: JavaScript statements can be grouped together in code blocks, inside curly brackets {...}. The purpose of code blocks is to define statements to be executed together. One place you will find statements grouped together in blocks, is in JavaScript functions.
+
+14) JavaScript Keywords: JavaScript statements often start with a keyword to identify the JavaScript action to be performed. JavaScript keywords are reserved words. Reserved words cannot be used as names for variables. 
+   a) var: Declares a variable
+   b) let: Declares a block variable
+   c) const: Declares a block constant
+   d) if: Marks a block of statements to be executed on a condition
+   e) switch: Marks a block of statements to be executed in different cases
+   f) for: Marks a block of statements to be executed in a loop
+   g) function: Declares a function
+   h) return: Exits a function
+   i) try: Implements error handling to a block of statements
+
+15) JavaScript Values: The JavaScript syntax defines two types of values 
+   a) Fixed values: Fixed values are called Literals.
+   b) Variable values: Variable values are called Variables.
+
+16) JavaScript Literals: The two most important syntax rules for fixed values are:
+   a) Numbers are written with or without decimals.
+   b) Strings are text, written within double or single quotes.
+
+17) JavaScript Variables: In a programming language, variables are used to store data values. JavaScript uses the keywords var, let and const to declare variables. An equal sign is used to assign values to variables.
+
+18) JavaScript Expressions: An expression is a combination of values, variables, and operators, which computes to a value. The computation is called an evaluation. For example, 5 * 10 evaluates to 50. Expressions can also contain variable values like (X * 10). The values can be of various types, such as numbers and strings.
+
+19) JavaScript Keywords: JavaScript keywords are used to identify actions to be performed. The let keyword tells the browser to create variables. The var keyword also tells the browser to create variables.
+
+20) JavaScript Comments: Not all JavaScript statements are "executed". Code after double slashes // or between /* and / is treated as a comment. Comments are ignored, and will not be executed.
+
+21) JavaScript Identifiers/Names: Identifiers are JavaScript names. Identifiers are used to name variables and keywords, and functions. The rules for legal names are the same in most programming languages. Subsequent characters may be letters, digits, underscores, or dollar signs. Numbers are not allowed as the first character in names. This way JavaScript can easily distinguish identifiers from numbers. A JavaScript name must begin with:
+   a) A letter (A-Z or a-z)
+   b) A dollar sign ($)
+   c) Or an underscore (_) 
+
+22) JavaScript is Case Sensitive: All JavaScript identifiers are case sensitive. The variables 'lastName' and 'lastname', are two different variables. JavaScript does not interpret LET or Let as the keyword let.
+
+23) JavaScript and Camel Case: Hyphens are not allowed in JavaScript. They are reserved for subtractions ( - ). 
+   a) Underscore: first_name, last_name, master_card, inter_city.
+   b) Upper Camel Case: FirstName, LastName, MasterCard, InterCity.
+   c) Lower Camel Case: firstName, lastName, masterCard, interCity.
+
+
+
+
+
+
+
+*/
